ba1723f7d098d6646cb7c4585084b50e701cc90c
Berke Batur <aberkebatur@gmail.com>
2021-08-22 23:46:09 +0300

    finished the project

--- src/store/getters.js
+++ src/store/getters.js
@@ -6,13 +6,6 @@ export const getLessons = (state) => {
     return state.lessons
 }
 
-export const checkTrue = (state,payload) => {
-    if (payload.number > 20) 
-      return "bigger"
-     else 
-      return "lesser"
-}
-
 export const studentByNumber = (state) => {
     return state.studentByNumber
 }
